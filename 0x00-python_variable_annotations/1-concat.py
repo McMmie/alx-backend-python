5862b88e581ae24fbf4bdfe8f773e32bb4fdca1e
McMmie <tharausamuel01@gmail.com>
2024-03-15 21:18:43 +0000

    added a concat function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+this module handles a concatenation function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    a type-annotated function that returns a concatenated string
+    """
+
+    return str1 + str2
