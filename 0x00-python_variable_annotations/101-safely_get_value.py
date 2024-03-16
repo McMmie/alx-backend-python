b34850c4d3554e9a14c5480993b0e691a6a7479d
McMmie <tharausamuel01@gmail.com>
2024-03-16 01:51:20 +0000

    initial commit

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This is a module that provides a function for safely getting a value from
+    a mapping.
+    """
+    from typing import Any, Mapping, Union, TypeVar
+
+    T = TypeVar('T')
+    Res = Union[Any, T]
+    Def = Union[T, None]
+
+def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
+    """
+    documentation
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This is a module that provides a function for zooming in on a tuple.
+"""
+from typing import List, Tuple
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    documentation
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(int(factor))
+    ]
+    return zoomed_in
