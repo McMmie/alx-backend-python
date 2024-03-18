4b343988c6cac98b7472c2e968ae8a8fbf5cb559
McMmie <tharausamuel01@gmail.com>
2024-03-18 23:54:30 +0000

    added type annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,11 +2,12 @@
 """
 documentation
 """
-wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     documentation needed
     """
