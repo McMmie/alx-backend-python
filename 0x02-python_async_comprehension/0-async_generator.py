26734e6b2d2f9f5f2fe96e995eb8d39965eec43b
McMmie <tharausamuel01@gmail.com>
2024-03-19 10:54:41 +0000

    refactoring code

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,6 +10,6 @@ async def async_generator():
     """
     generates a random number between 0 and 10
     """
-    for i in range(10):
+    for i in range(11):
         await asyncio.sleep(1)
-        yield uniform(0, 11)
+        yield uniform(0, 10)
